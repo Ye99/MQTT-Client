9d00ef28bac4844dbd42e0144b9c92be51f2a065
Ye Zhang <mr.yezhang@gmail.com>
2020-05-03 21:23:07 -0700

    Add timestamp to received messages.

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
+from datetime import datetime
+
 import paho.mqtt.client as mqtt
 
 
+# API document see: https://github.com/eclipse/paho.mqtt.python#subscribe-unsubscribe
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
+    print("Connected with result code {result_code}.\t\t{time_stamp}".format(
+        result_code=str(rc), time_stamp=get_local_timestamp()))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe([("network_watchdog_status_topic", 0), ("smart_uv_light_status_topic", 0)])
 
 
+def get_local_timestamp() -> str:
+    return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+    print("{topic}\t{payload}\t{time_stamp}".format(
+        topic=msg.topic, payload=str(msg.payload), time_stamp=get_local_timestamp()))
 
 
 client = mqtt.Client()
