503132795dd765fbf8f15b74aa18c6f92c0873e6
Ye Zhang <mr.yezhang@gmail.com>
2020-05-03 23:10:53 -0700

    Append messages to log.txt.

--- main.py
+++ main.py
@@ -6,11 +6,18 @@ with open('.credentials') as f:
     credentials = ujson.load(f)
 
 
+def append_to_log(text: str) -> None:
+    with open("log.txt", "a") as log_file:
+        log_file.write(text + '\n')
+
+
 # API document see: https://github.com/eclipse/paho.mqtt.python#subscribe-unsubscribe
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code {result_code}.\t\t{time_stamp}".format(
-        result_code=str(rc), time_stamp=get_local_timestamp()))
+    message = "Connect result code {result_code}.\t\t{time_stamp}".format(
+        result_code=str(rc), time_stamp=get_local_timestamp())
+    print(message)
+    append_to_log(message)
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
@@ -23,8 +30,10 @@ def get_local_timestamp() -> str:
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print("{topic}\t{payload}\t{time_stamp}".format(
-        topic=msg.topic, payload=str(msg.payload), time_stamp=get_local_timestamp()))
+    message = "{topic}\t{payload}\t{time_stamp}".format(
+        topic=msg.topic, payload=str(msg.payload), time_stamp=get_local_timestamp())
+    print(message)
+    append_to_log(message)
 
 
 client = mqtt.Client()
